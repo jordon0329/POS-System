76ab6d4cc1912056a78499db15024dae797ec326
superdeveloper717 <falviolegros717@gmail.com>
2023-11-18 11:44:14 +0900

    tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   content: ['./src/renderer/**/*.{js,jsx,ts,tsx}'],
-=======
-  content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
   darkMode: false, // or 'media' or 'class'
->>>>>>> d1125c779f16951a4cfa71d4875bbda502fe67cc
-=======
-  content: [],
->>>>>>> parent of 8383bc2 (first commit)
   theme: {
     extend: {
       colors: {
